1dea939c0f8de73c6cf864563dc622197f19db67
weizidong <277195991@qq.com>
2015-05-27 18:11:33 +0800

    ...

--- src/main/webapp/app/controller/order/list.js
+++ src/main/webapp/app/controller/order/list.js
@@ -1,30 +1,36 @@
 "use strict";
 
-define([ 'app', 'services/api/meal/mealApi', 'services/api/order/orderApi', 'css!style/order/list','services/api/account/accountApi','directive/dialog/dialog' ], function(app) {
+define([ 'app', 'services/api/meal/mealApi', 'services/api/order/orderApi', 'css!style/order/list', 'services/api/account/accountApi', 'directive/dialog/dialog' ], function(app) {
 
-	app.register.controller('controller.order.list', [ '$scope', '$rootScope', '$location', 'mealApi', 'orderApi','accountApi','dialogService', function($scope, $rootScope, $location, mealApi, orderApi,accountApi,dialogService) {
-		orderApi.getMealOrder({
-			success:function(data){
-				console.log(data);
-				$scope.order = data;
-			}
-		});
+	app.register.controller('controller.order.list', [ '$scope', '$rootScope', '$location', 'mealApi', 'orderApi', 'accountApi', 'dialogService', function($scope, $rootScope, $location, mealApi, orderApi, accountApi, dialogService) {
+		load();
+		function load(){
+			orderApi.getMealOrder({
+				success : function(data) {
+					console.log(data);
+					$scope.order = data;
+					if(!data.status){
+						setTimeout(load, 5000);
+					}
+				}
+			});
+		}
+		
 		$scope.remark = function(account) {
 			dialogService.input({
-				hasNull:true,
+				hasNull : true,
 				validate : 'text',
-				input:account.remarkName,
-				confirm:function(){
+				input : account.remarkName,
+				confirm : function() {
 					account.remarkName = $scope.opts.input;
 					accountApi.setRemarkName({
-						data:{
-							accountId:account.id,
-							remarkName:account.remarkName
+						data : {
+							accountId : account.id,
+							remarkName : account.remarkName
 						}
 					});
 				}
 			});
 		};
-		
 	} ]);
 });
--- src/test/java/meal_order/AddMeal.java
+++ src/test/java/meal_order/AddMeal.java
@@ -18,20 +18,20 @@ public class AddMeal {
 
 	@Test
 	public void addMeal() {
-		mealDao.create(Meal.generate("水煮鱼", 15.0));
-		mealDao.create(Meal.generate("茶树菇炒腊肉", 15.0));
-		mealDao.create(Meal.generate("西芹猪肚", 15.0));
-		mealDao.create(Meal.generate("红油肚丝", 15.0));
-		mealDao.create(Meal.generate("酸菜猪肚", 15.0));
-		mealDao.create(Meal.generate("酸菜鱼", 15.0));
-		mealDao.create(Meal.generate("泡椒凤爪", 15.0));
-		mealDao.create(Meal.generate("老干妈炒牛肉", 15.0));
-		mealDao.create(Meal.generate("野山椒牛肉丝", 15.0));
-		mealDao.create(Meal.generate("水煮牛肉", 18.0));
-		mealDao.create(Meal.generate("泡椒猪腰", 18.0));
-		mealDao.create(Meal.generate("红油猪耳", 15.0));
-		mealDao.create(Meal.generate("虾饺", 12.0));
-		mealDao.create(Meal.generate("肉丝面", 10.0));
+		mealDao.create(Meal.generate("青岛啤酒", 5.0));
+		// mealDao.create(Meal.generate("茶树菇炒腊肉", 15.0));
+		// mealDao.create(Meal.generate("西芹猪肚", 15.0));
+		// mealDao.create(Meal.generate("红油肚丝", 15.0));
+		// mealDao.create(Meal.generate("酸菜猪肚", 15.0));
+		// mealDao.create(Meal.generate("酸菜鱼", 15.0));
+		// mealDao.create(Meal.generate("泡椒凤爪", 15.0));
+		// mealDao.create(Meal.generate("老干妈炒牛肉", 15.0));
+		// mealDao.create(Meal.generate("野山椒牛肉丝", 15.0));
+		// mealDao.create(Meal.generate("水煮牛肉", 18.0));
+		// mealDao.create(Meal.generate("泡椒猪腰", 18.0));
+		// mealDao.create(Meal.generate("红油猪耳", 15.0));
+		// mealDao.create(Meal.generate("虾饺", 12.0));
+		// mealDao.create(Meal.generate("肉丝面", 10.0));
 		// mealDao.create(Meal.generate("梅菜扣肉", 13.0));
 		// mealDao.create(Meal.generate("鼓汁排骨", 13.0));
 		// mealDao.create(Meal.generate("土豆鸭", 13.0));
